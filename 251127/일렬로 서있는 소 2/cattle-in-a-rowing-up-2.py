d6dad87941cde684eda7375a5c968a6ea8aa74b3
lsm <124768198+sml1323@users.noreply.github.com>
2025-11-27 14:12:25 +0900

    Title: 일렬로 서있는 소 2, Time: 52ms, Memory: 17MB, Status: Passed - Codetree

--- 251127/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 251127/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -8,7 +8,7 @@ A_length = len(A)
 for i in range(A_length):
     for j in range(i + 1, A_length):
         for k in range(j + 1, A_length):
-            if A[i] < A[j] < A[k]:
+            if A[i] <= A[j] <= A[k]:
                 sol += 1
 
 print(sol)
