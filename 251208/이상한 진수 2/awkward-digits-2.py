7f9fd6ace3f293878649de8bcf854871f7ea235b
lsm <124768198+sml1323@users.noreply.github.com>
2025-12-08 17:58:17 +0900

    Title: 이상한 진수 2, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251208/이상한 진수 2/awkward-digits-2.py
+++ 251208/이상한 진수 2/awkward-digits-2.py
@@ -5,20 +5,21 @@ a = input()
 
 # 앞의 자릿수부터 0 -> 1로 바꿈, 안된다면 그대로 10진수 변환하여 출력
 
-new_number = []
-count = 0
-for number in a:
-
-    if number == "0" and count < 1:
-        new_number.append("1")
-        count += 1
-    else:
-        new_number.append(number)
-
+new_number = list(a)
+changed = False
+a_len = len(a)
+for i in range(len(new_number)):
+    if new_number[i] == '0':
+        new_number[i] = '1'
+        changed = True
+        break
+    
+if not changed:
+    new_number[-1] = "0"
 num = 0
 for i in range(len(new_number)):
     num = num * 2 + int(new_number[i])
-if count == 0:
-    print(0)
-else:
-    print(num)
+
+
+
+print(num)
