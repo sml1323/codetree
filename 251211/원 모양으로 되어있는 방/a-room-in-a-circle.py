ee070fe4e274883055a6d89fd6e4c190d9990849
lsm <124768198+sml1323@users.noreply.github.com>
2025-12-11 14:40:23 +0900

    Title: 원 모양으로 되어있는 방, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251211/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -0,0 +1,39 @@
+n = int(input())
+a = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+import sys
+
+human_dict = {}
+for i in range(1, n + 1):
+    human_dict[i] = a[i - 1]
+
+# 1에서 시작: (1 - 2) x human_dict[2], (1 - 3) x human_dict[3]...
+# 만약 이동 방이 번호가 작다면: 2 -> 1 : 3 4 5 1 거리 4. 
+
+def calculate(start_room, moving_room):
+    if start_room > moving_room:
+        dist = n - moving_room       
+    else:
+        dist = abs(start_room - moving_room)    
+    return  dist * human_dict[moving_room]
+
+
+distance = sys.maxsize
+
+def moving_distance(start_room):
+    move_distance = 0
+    for i in range(1,n + 1):
+        move_distance += calculate(start_room, i)
+    return move_distance
+
+for start_room in range(1, n + 1):
+    distance = min(moving_distance(start_room), distance)
+
+print(distance)
+    
+    
+    
+
+
+
