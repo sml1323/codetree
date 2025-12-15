9dd9c34935ed3fa881dd98ce2dd642a0ce20232c
lsm <124768198+sml1323@users.noreply.github.com>
2025-12-15 10:46:38 +0900

    Title: 인접하지 않은 2개의 수, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251215/인접하지 않은 2개의 수/two-non-adjacent-numbers.py
+++ 251215/인접하지 않은 2개의 수/two-non-adjacent-numbers.py
@@ -4,22 +4,14 @@ numbers = list(map(int, input().split()))
 # Please write your code here.
 
 
-sorted_numbers = sorted(numbers, reverse=True)
+first_max = max(numbers)
+f_idx = numbers.index(first_max)
+second_max = 0
 
+for i in range(len(numbers)):
+    if abs(f_idx - i) > 1:
+        second_max = max(second_max, numbers[i])
 
-# index dict
-index_dict = {}
-for i in sorted_numbers:
-    a = numbers.index(i)
-    index_dict[a] = i
-
-max_idx = numbers.index(sorted_numbers[0])
-next_idx = 0
-
-for i, v in index_dict.items():
-    if i != max_idx and abs(max_idx - i) > 1:
-        next_idx = i
-        break
-print(index_dict[max_idx] + index_dict[next_idx])
+print(first_max + second_max)
 
 
