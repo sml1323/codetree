794b04cd40bf44fcb7ef0072c02d389d9420a9e8
lsm <124768198+sml1323@users.noreply.github.com>
2025-12-24 16:59:53 +0900

    Title: 특정 수와 근접한 합, Time: 65ms, Memory: 17MB, Status: Passed - Codetree

--- 251224/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 251224/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -6,7 +6,7 @@ arr = list(map(int, input().split()))
 # 두개를 제외한 모든 합 리스트 생성
 sums = []
 for i in range(N - 1):
-    for j in range(i, N):
+    for j in range(i + 1, N):
         sums.append(abs((sum(arr) - (arr[i] + arr[j])) - S))
 
 sums = sorted(sums)
