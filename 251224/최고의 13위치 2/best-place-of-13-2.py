d90a128687fe68066eaaa88f576b2983f96f89e2
lsm <124768198+sml1323@users.noreply.github.com>
2025-12-24 16:45:26 +0900

    Title: 최고의 13위치 2, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

--- 251224/최고의 13위치 2/best-place-of-13-2.py
+++ 251224/최고의 13위치 2/best-place-of-13-2.py
@@ -6,12 +6,10 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 def in_range(r,c):
     return 0 <= r < n and 0 <= c < n
 
-count_dict = {
-    0: 0,
-    1: 0,
-    2: 0,
-    3: 0
-}
+# 점수, 행, 열
+grid_list = []
+
+
 
 def count_one(r,c):
     count = 0
@@ -24,29 +22,17 @@ def count_one(r,c):
 for r in range(n):
     for c in range(n):
         if in_range(r, c+2):
-            count_dict[count_one(r,c)] += 1
+            grid_list.append((count_one(r,c), r, c))
+
 sol = 0
-count = 0
+grid_list = sorted(grid_list, reverse=True)
 
-if count_dict[3] >= 2:
-    sol = 6
-elif count_dict[3] == 1:
-    if count_dict[2]:
-        sol = 5
-    elif count_dict[1]:
-        sol = 4
-    else:
-        sol = 3
-else:
-    if count_dict[2] >= 2:
-        sol = 4
-    elif count_dict[2]:
-        if count_dict[1]:
-            sol = 3
-        else:
-            sol = 2
+for i in range(len(grid_list) - 1):
+    for j in range(1, len(grid_list)):
+        if grid_list[i][1] != grid_list[j][1] or abs(grid_list[i][2] - grid_list[j][2]) >= 3:
+            sol = max(grid_list[i][0] + grid_list[j][0], sol)
+            
+
 print(sol)
-
-    
 
 
